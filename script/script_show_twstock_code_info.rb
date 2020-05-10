233efbb6f7c0137a5afc796ad8297b13ce2f1908
yuya sato <ysato.5654@gmail.com>
2020-05-10 21:00:09 +0800

    update script - optimize optparse

--- script/script_show_twstock_code_info.rb
+++ script/script_show_twstock_code_info.rb
@@ -8,7 +8,7 @@ require File.dirname(File.realpath(__FILE__)) + '/../lib/twstock/stock_code'
 
 Year    = '2020'
 Month   = 'May'
-Day     = '10'
+Day     = '10a'
 Build   = [Day, Month, Year].join(' ')
 
 Version = Build + ' ' + '(' + 'twstock-finance' + ' ' + 'v' + Twstock::Finance::VERSION + ')'
@@ -27,63 +27,37 @@ def parse_option
 
     opt.parse(ARGV)
 
-    option
-end
-
-if $0 == __FILE__
-
-    # parse option
-    begin
-        option = parse_option
-
-    # display help
-    rescue SystemExit => e
-        exit(0)
-
-    # invalid option (undefined option)
-    rescue Exception => e
-        STDERR.puts "#{__FILE__}: #{e} (--help will show valid options)"
-        exit(0)
-    end
-
-    # invalid option (no option)
+    # no necessary option
     if option[:show].nil?
-        STDERR.puts "#{__FILE__}: no option (--help will show valid options)"
-        exit(0)
+        STDERR.puts "#{__FILE__}: no option: --show (--help will show valid options)"
+        raise SystemExit
     end
 
     # invalid option (show setting)
-    unless SHOW.include?(option[:show])
-        STDERR.puts "#{__FILE__}: invalid option (--help will show valid options)"
-        exit(0)
-    end
+    raise OptionParser::InvalidArgument unless SHOW.include?(option[:show])
 
-    # invalid option (language setting)
-    unless LANGUAGE.include?(option[:lang])
-        STDERR.puts "#{__FILE__}: invalid option (--help will show valid options)"
-        exit(0)
-    end
+    # invalid argument (language setting)
+    raise OptionParser::InvalidArgument unless LANGUAGE.include?(option[:lang])
 
-    filename = File.basename(__FILE__).gsub(File.extname(__FILE__), '')
+    option
+end
 
-    STDOUT.puts filename + ' ' + Version
-    STDOUT.puts
-
+def show_twstock_code_info(show:, lang:)
     begin
         twstock = Twstock::StockCode.new
     rescue Exception => e
         STDERR.puts "#{__FILE__}:#{__LINE__}: #{e.message} (#{e.class})"
-        exit(0)
+        return
     end
 
     # header
-    header = Twstock::StockCode::KEY[option[:lang].to_sym]
+    header = Twstock::StockCode::KEY[lang.to_sym]
     STDOUT.puts header.join("\s\|\s")
     STDOUT.puts [header.map { |e| "-" * e.length }].join("\s\|\s")
 
     # body
     twstock.codes_info.each do |code_info|
-        case option[:show]
+        case show
         when SHOW[0]
             STDOUT.puts code_info.values.join("\s\|\s")
         when SHOW[1]
@@ -96,5 +70,29 @@ if $0 == __FILE__
             end
         end
     end
+end
+
+if $0 == __FILE__
+
+    # parse option
+    begin
+        option = parse_option
+
+    # display help or no necessary option fail
+    rescue SystemExit => e
+        exit(0)
+
+    # invalid option (undefined option)
+    rescue Exception => e
+        STDERR.puts "#{__FILE__}: #{e} (--help will show valid options)"
+        exit(0)
+    end
+
+    filename = File.basename(__FILE__).gsub(File.extname(__FILE__), '')
+
+    STDOUT.puts filename + ' ' + Version
+    STDOUT.puts
+
+    show_twstock_code_info(:show => option[:show], :lang => option[:lang])
 
 end
